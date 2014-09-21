8b00061cdce35ee7f96886a83cf00b37b8646174
heartnest <granwit@gmail.com>
2014-09-21 18:47:08 +0200

    tables layout is better now and connected with json, tab bar will be connected to tables

--- fastmenu/TableViewController.m
+++ fastmenu/TableViewController.m
@@ -11,9 +11,10 @@
 #import "MenuViewController.h"
 
 
-@interface TableViewController ()
+@interface TableViewController ()<UITabBarDelegate>
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-
+@property (strong,nonatomic) NSArray *tables;
+@property (weak, nonatomic) IBOutlet UITabBar *tabbar;
 
 @end
 
@@ -23,7 +24,7 @@
     [super viewDidAppear:animated];
     
     [self.scrollView setScrollEnabled:YES];
-    [self.scrollView setContentSize:CGSizeMake(320, 1200)];
+   // [self.scrollView setContentSize:CGSizeMake(320, 1200)];
 }
 
 
@@ -31,50 +32,24 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+//    UITabBarController *tabBarController = (UITabBarController*)[UIApplication sharedApplication].keyWindow.rootViewController ;
+    [self.tabbar setDelegate:self];
+    //get table info
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"tables" ofType:@"json"];
+    NSData *data = [[NSFileManager defaultManager] contentsAtPath:filePath];
+    self.tables = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
 
+    
+    [self createTableButtonsFromArray:self.tables];
 
-    
-    //get var,create table buttons
-    double width = self.scrollView.frame.size.width;
-    
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    button.backgroundColor = [UIColor greenColor];
-    [button addTarget:self
-               action:@selector(didPressButton:)
-     forControlEvents:UIControlEventTouchUpInside];
-    
-    [button setTitle:@"Show View" forState:UIControlStateNormal];
-    button.frame = CGRectMake(width/2 + 1, 100, 7*width/16, 100.0);
-
-    [self.scrollView addSubview:button];
-    
-
-    UIButton *button2 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    button2.backgroundColor = [UIColor greenColor];
-    [button2 addTarget:self
-               action:@selector(didPressButton2:)
-     forControlEvents:UIControlEventTouchUpInside];
-    
-    [button2 setTitle:@"Show View" forState:UIControlStateNormal];
-    button2.frame = CGRectMake(1, 100, 7*width/16, 100.0);
-    
-    [self.scrollView addSubview:button2];
-    
- 
-    
-    
-    //xxxxx
-    
-    UITextView *textView = [[UITextView alloc] init];
-    textView.frame = CGRectMake(0.0,0.0,28.0,21.0);
-    [textView setReturnKeyType: UIReturnKeyDone];
-    textView.backgroundColor = NULL;
-    
-    textView.text = @"aaa";
-    
-    [button addSubview: textView];
+}
+-(void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item{
+    NSLog(@"aa %@",item.title);
     
 }
+
+#pragma mark - UIButton outlets
+
 
 
 
@@ -85,7 +60,7 @@
     [self performSegueWithIdentifier:@"occupiedtable" sender:self];
 }
 
-- (void)didPressButton:(UIButton *)sender
+- (void)didPressTableHasOrder:(UIButton *)sender
 {
  
     UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
@@ -96,7 +71,7 @@
  [self.navigationController pushViewController:viewController animated:YES];
 }
 
-- (void)didPressButton2:(UIButton *)sender
+- (void)didPressTableNeedsOrder:(UIButton *)sender
 {
     
     UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
@@ -107,22 +82,84 @@
     [self.navigationController pushViewController:viewController animated:YES];
 }
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-
-        
-        if ([segue.identifier isEqualToString:@"occupiedtable"]) {
-            
-//            if ([segue.destinationViewController respondsToSelector:@selector(setImageURL:)]) {
-            
-
-                //NSLog(@"ccc %@", photo.imageURL);
-          //  }
+-(NSArray *)filterTablesWithArray:(NSArray *)arr andState:(NSString *)state{
+    NSMutableArray *result = [[NSMutableArray alloc]init];
+    for (NSDictionary *table in arr) {
+         NSString *tablestate = [table objectForKey:@"state"];
+        if([state isEqualToString:tablestate]){
+            [result addObject:table];
         }
-    
-    
+            
+    }
+    return [result copy];
 }
 
+
+-(void) createTableButtonsFromArray:(NSArray *) arr{
+    
+    //get var,create table buttons
+    int margintop = 10;
+    int marginleft = 10;
+    int count = 0;
+    double width = self.scrollView.frame.size.width;
+    double ypos = margintop;
+    double boxwidth = width*6/16;
+    double boxheight = 100;
+    
+    
+    for (NSDictionary *table in arr) {
+        
+        int tableid = [[table  objectForKey:@"tableid"] intValue];
+        NSString *tablestate = [table objectForKey:@"state"];
+        
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        
+        if ([tablestate isEqualToString:@"free"]) {
+            button.backgroundColor = [UIColor greenColor];
+            [button addTarget:self
+                       action:@selector(didPressTableNeedsOrder:) forControlEvents:UIControlEventTouchUpInside];
+        }else if ([tablestate isEqualToString:@"busy"]) {
+            button.backgroundColor = [UIColor redColor];
+            [button addTarget:self
+                       action:@selector(didPressTableHasOrder:) forControlEvents:UIControlEventTouchUpInside];
+        }else{
+            button.backgroundColor = [UIColor yellowColor];
+            [button addTarget:self
+                       action:@selector(didPressTableNeedsOrder:) forControlEvents:UIControlEventTouchUpInside];
+        }
+        
+        button.tag = 2000+tableid;
+        
+        //make the buttons content appear in the top-left
+        [button setContentHorizontalAlignment:UIControlContentHorizontalAlignmentLeft];
+        [button setContentVerticalAlignment:UIControlContentVerticalAlignmentTop];
+        
+        //move text 10 pixels down and right
+        [button setTitleEdgeInsets:UIEdgeInsetsMake(10.0f, 10.0f, 0.0f, 0.0f)];
+        
+        button.titleLabel.lineBreakMode = NSLineBreakByCharWrapping;
+        [button setTitle:@"Show View\nlalala" forState:UIControlStateNormal];
+        
+        if (count%2 == 1) {
+            button.frame = CGRectMake(width/2 + 1, ypos, boxwidth, boxheight);
+            ypos += (boxheight+margintop);
+        }else
+            button.frame = CGRectMake(marginleft, ypos, boxwidth, boxheight);
+        
+        [self.scrollView addSubview:button];
+        
+        count++;
+
+    }
+    
+    
+    [self.scrollView setScrollEnabled:YES];
+    [self.scrollView setContentSize:CGSizeMake(width, ypos)];
+}
+
+#pragma marks - tabbar select event -
+//- (void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController{
+//    NSLog(@"a");
+//}
 
 @end
